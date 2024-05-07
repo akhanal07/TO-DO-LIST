e9498ce9f40a5b0a37f146b9e3ae5f2d56f74e0e
Ayush Khanal <7030ak58@gmail.com>
2024-05-07 18:38:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+let btn = document.querySelector("button");
+let inp = document.querySelector("input");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", function() {
+  let item = document.createElement("li");
+  item.innerText = inp.value;
+
+  let deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "Delete";
+  deleteBtn.classList.add("delete");
+  item.appendChild(deleteBtn);
+
+  // Check if ul exists before appending
+  if (ul) {
+    ul.appendChild(item);
+    ul.appendChild(document.createElement("br")); // Add line break
+  } else {
+    console.error("ul element not found");
+  }
+
+  inp.value = "";
+});
+
+ul.addEventListener("click", function(event) {
+  if (event.target.nodeName === "BUTTON") {
+    // Check if the parent element exists before removing
+    if (event.target.parentElement) {
+      let element = event.target.parentElement;
+      element.remove();
+    }
+  }
+});
+
+
+// let Delbtns = document.querySelectorAll(".delete");
+// for (Delbtn of Delbtns){
+//     Delbtn.addEventListener("click",function(){
+//         let par = this.parentElement;
+//         par.remove();
+//         console.log(par);
+//     })
+// }
